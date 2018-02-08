50bded2d99a8bc08b9576ff3fbaea0b254a431fa
LuisSevern <36269765+LuisSevern@users.noreply.github.com>
2018-02-08 18:55:16 +0100

    Update principal.java

--- ejercicioEclipse/src/ejercicioEclipse/principal.java
+++ ejercicioEclipse/src/ejercicioEclipse/principal.java
@@ -4,7 +4,8 @@ public class principal {
 
 	public static void main(String[] args) {
 	System.out.println("HOLA");
-		System.out.println("¿Qué tal?");
+	System.out.println("¿Qué tal?");
+		System.out.println("¿Me has oido?");
 
 	}
 
